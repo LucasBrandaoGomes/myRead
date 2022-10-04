f6b139b21e83209e15b960234a2b9443b8ad8ad7
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2022-10-03 22:48:22 -0300

    feat: create user repository

---
+++ src/repositories/userRepository.ts
@@ -0,0 +1,10 @@
+import { prisma } from "../database/database";
+import { Users } from "@prisma/client";
+
+export async function findUserByEmail(email:string): Promise < Users | null> {
+   return await prisma.users.findUnique({where: {email:email}})
+}
+
+export async function inserNewUser(email:string, password: string) {
+    await prisma.users.create({data: {email, password}})
+}
