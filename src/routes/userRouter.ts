c32e749b063712f6722defd708bfe2a228260be5
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2022-10-03 23:15:33 -0300

    feat: add signin route

---
+++ src/routes/userRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import * as schemas from "../schemas/schemas";
+import * as controller from "../controllers/userControllers";
+import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware";
+
+const userRouter = Router()
+
+userRouter.post('/sign-up', validateSchemaMiddleware(schemas.signUpSchema), controller.signUp)
+userRouter.post('/sign-in', validateSchemaMiddleware(schemas.signInSchema), controller.signIn)
+
+
+export default userRouter
