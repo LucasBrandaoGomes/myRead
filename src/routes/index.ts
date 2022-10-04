7dc477c6c15af7b8ee000a395bbe9c5b7cdeb330
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2022-10-04 10:43:14 -0300

    feat:  import use router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import bookRouter from "./bookRouter";
 import userRouter from "./userRouter";
 
 const router = Router()
 
 router.use(userRouter);
+router.use(bookRouter)
 
 export default router
