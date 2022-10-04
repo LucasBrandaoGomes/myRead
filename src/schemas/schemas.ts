88a4d69bedcdbecb85024c2c09a28ac63bdad21d
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2022-10-03 23:15:21 -0300

    feat: create signin schema

---
+++ src/schemas/schemas.ts
@@ -0,0 +1,17 @@
+import joi from 'joi'
+import { UserBodyData, UserSignInBodyData } from '../types/userType';
+
+const signUpSchema = joi.object<UserBodyData>({
+    email: joi.string().email().required(),
+    password: joi
+    .string()
+    .required(),
+    passwordConfirmation: joi.any().valid(joi.ref('password')).required()
+  });
+
+const signInSchema = joi.object<UserSignInBodyData>({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+});
+
+export { signUpSchema, signInSchema }
