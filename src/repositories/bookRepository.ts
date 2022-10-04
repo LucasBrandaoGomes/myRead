64fe330230754a5dee8b3ea5a8d010edadeccd2d
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2022-10-04 17:41:22 -0300

    feat: create query to get books by search query param

--- src/repositories/bookRepository.ts
+++ src/repositories/bookRepository.ts
@@ -5,6 +5,24 @@ export async function findAllBooks() : Promise < Books[] > {
     return await prisma.books.findMany()
 }
 
+export async function findAllBooksBySearch(search:string) : Promise < Books[] > {
+    return await prisma.books.findMany({
+        where: {
+            OR: [
+                {title:{
+                    contains: `${search}`,
+                    mode: 'insensitive',
+                }},
+                {author: {
+                    contains: `${search}`,
+                    mode: 'insensitive',
+                }},
+                 ],
+        }
+        
+    })
+}
+
 export async function findBookById(id:number) : Promise < Books > {
     return await prisma.books.findUnique({where:{id:id}})
 }
