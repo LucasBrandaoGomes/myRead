37f0345b7c41c35c930229c5f5b8b0fddf1612da
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2022-10-03 23:14:49 -0300

    feat: add signin type

---
+++ src/types/userType.ts
@@ -0,0 +1,9 @@
+import { Users } from "@prisma/client";
+
+export type UserInsertData = Omit<Users, 'id'>;
+
+export type UserBodyData = {email:string, password:string, passwordConfirmation: string };
+
+export type UserSignInBodyData = {email:string, password:string};
+
+export type UserResponseData = Omit<Users, 'userId'>;
