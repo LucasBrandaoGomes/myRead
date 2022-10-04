2573dfd1556e5e986e4def2d625f195c29802238
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2022-10-04 11:29:31 -0300

    feat: create get book by id query

--- src/repositories/bookRepository.ts
+++ src/repositories/bookRepository.ts
@@ -4,3 +4,7 @@ import { Books } from "@prisma/client";
 export async function findAllBooks() : Promise < Books[] > {
     return await prisma.books.findMany()
 }
+
+export async function findBookById(id:number) : Promise < Books > {
+    return await prisma.books.findUnique({where:{id:id}})
+}
