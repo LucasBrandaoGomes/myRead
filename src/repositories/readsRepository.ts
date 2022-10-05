326b34aa319914fd2f2b06fdcc7dd9ec351617d1
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2022-10-05 12:40:34 -0300

    feat: create update function

--- src/repositories/readsRepository.ts
+++ src/repositories/readsRepository.ts
@@ -2,10 +2,10 @@ import { UserBook } from "@prisma/client";
 import { prisma } from "../database/database";
 import { UserBookInsertData } from "../types/userBookType";
 
-export async function insertNewRead(data: UserBookInsertData) {
+export async function insertNewRead(data: UserBookInsertData) : Promise <void> {
     await prisma.userBook.create({data})
 }
-export async function findUniqueUserBook(userId:number, bookId:number) : Promise < void | UserBook > {
+export async function findUniqueUserBook(userId:number, bookId:number) : Promise < UserBook > {
     const result = await prisma.userBook.findUnique(
         {where: 
             {userId_bookId: {
@@ -16,4 +16,15 @@ export async function findUniqueUserBook(userId:number, bookId:number) : Promise
         },
     )
     return result
+}
+
+export async function updateReadPages(id:number, value: number) : Promise < void > {
+    await prisma.userBook.update({
+        where: {
+          id: id
+        },
+        data: {
+          readPages: value
+        },
+      })
 }
